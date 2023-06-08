d9c929382e2837dcd55844e0a8227958e79d1ace
Ganesh Maharnur <ganeshmaharnur11@gmail.com>
2023-06-08 09:37:40 +0530

    Hello from backend

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const middlerware = (req, res, next) => {
+    console.log(`HI from middleWare`);
+    next();
+}
+
+app.get('/', (req, res) => {
+    res.send(`Hello from server`);
+});
+
+app.get('/about', middlerware, (req, res) => {
+    res.send(`Hello about page from server`);
+});
+
+app.get('/contact', (req, res) => {
+    res.send(`Hello contact page from server`);
+});
+
+app.get('/signin', (req, res) => {
+    res.send(`Hello login page from server`);
+});
+
+app.get('/signup', (req, res) => {
+    res.send(`Hello registraion page from server`);
+});
+
+app.listen(4000, ()=> {
+    console.log(`server is running`)
+})

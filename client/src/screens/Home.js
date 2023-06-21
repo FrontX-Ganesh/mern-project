8c6b51e22a065c176968486408c5dfa1aac9055a
Ganesh Maharnur <ganeshmaharnur11@gmail.com>
2023-06-21 19:54:16 +0530

    Nav Bar created also added tailwind css

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import NavBar from '../components/NavBar';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <NavBar />
+      <div>Home</div>
+    </>
   )
 }
 
